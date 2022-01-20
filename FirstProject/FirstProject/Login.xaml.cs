102b50eece9f7c32e7db0dc81a44f1a3202000a2
Марсель <@HOME-PC>
2022-01-20 23:35:10 +0300

    Добавьте файлы проекта.

---
+++ FirstProject/FirstProject/Login.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnRegistration_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegistrationPage());
+        }
+
+        private async void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Pages());
+        }
+    }
+}
---
+++ FirstProject/FirstProject/Pages.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pages : ContentPage
+    {
+        public Pages()
+        {
+            InitializeComponent();
+        }
+
+
+        private void projectList_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Navigation.PushAsync(new TableOsnova());
+        }
+    }
+}
